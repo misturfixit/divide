fcebac5e812156f047e50d03758c6e6353b403d2
misturfixit <japullins420@gmail.com>
2017-06-27 15:31:56 -0400

    divseeon.rb

---
+++ divseeon.rb
@@ -0,0 +1,21 @@
+def divide
+
+	print "Pick a number  :"   
+		a = gets.to_i
+
+	print "Pick Anudder Number, to divide by...and stuff   :"
+		b = gets.to_i
+
+
+		          if b == 0 
+		          	print "You Know YOu Can't do that"
+
+		          else
+
+		          	  c = a / b 
+
+                  end
+
+end
+
+       puts divide
